bd2698c54a612b6ea0c1fb12e3d237f24804b09f
Haroun Mamalo <mamalostar12311@gmail.com>
2025-08-28 20:10:22 +0800

    Implemented Edit and Destroy Post

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,8 @@ const { errorHandler } = require("./middleware/errorHandler");
 const { connectDb } = require("./connection/dbConnection");
 const app = express();
 const port =  process.env.port || 3000; //check if .port in .env is initialized, else run port 3000
+
 connectDb();
-
-
 app.use(express.json());
 app.use(errorHandler);
 app.listen(port, () => {
